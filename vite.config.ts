9bc0739f3f4401471f37721c49bf38a90bc58182
Sueda Çiçekli <suedacicekli@gmail.com>
2024-01-26 14:54:53 +0300

    added base to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "markdown-loader",
-      transform(code, id) {
-        if (id.slice(-3) === ".md") {
-          return `export default ${JSON.stringify(code)};`;
-        }
-      },
-    },
-  ],
+  plugins: [react()],
+  base: "https://zustand.sueda.me/",
 });
