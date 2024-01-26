2a4fb3633a5164b7708342d2f8ef0aac1a5adddc
Sueda Çiçekli <suedacicekli@gmail.com>
2024-01-26 04:15:25 +0300

    some trying

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "markdown-loader",
+      transform(code, id) {
+        if (id.slice(-3) === ".md") {
+          return `export default ${JSON.stringify(code)};`;
+        }
+      },
+    },
+  ],
   assetsInclude: ["**/*.md"],
 });
